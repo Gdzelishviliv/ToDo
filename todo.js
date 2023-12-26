7e40e9aec9374c2dafbcbac064e16ec10d67bd9f
ivane <gdzelishviliv@gmail.com>
2023-12-27 01:30:37 +0400

    added localStorage and addtime

--- todo.js
+++ todo.js
@@ -30,25 +30,21 @@ function updateCurrentDateTime() {
 setInterval(updateCurrentDateTime, 1000);
 
 updateCurrentDateTime();
-
 function addItem() {
     const input = document.getElementById("inputi");
     const inputValue = input.value.trim();
 
     if (inputValue.length > 1) {
         const li = document.createElement('li');
-
         const checkboxDiv = document.createElement('div');
-
         const checkbox = document.createElement('input');
         checkbox.type = 'button';
         checkbox.value = ''; // Set a value to make the button visible
-        
-
         const removeBtn = document.createElement('button');
         removeBtn.innerText = '';
         removeBtn.onclick = function () {
             li.remove();
+            updateLocalStorage();
         };
 
         checkboxDiv.appendChild(checkbox);
@@ -59,10 +55,110 @@ function addItem() {
 
         checkbox.addEventListener('click', function () {
             checkbox.classList.toggle('clicked');
+            updateLocalStorage();
         });
 
         document.getElementById("todoList").appendChild(li);
+        updateLocalStorage();
 
         input.value = "";
     }
+}
+
+function updateLocalStorage() {
+    const todoListItems = document.querySelectorAll("#todoList li");
+    const todoListArray = Array.from(todoListItems).map(item => item.outerHTML);
+    localStorage.setItem('todoList', JSON.stringify(todoListArray));
+}
+
+function loadItemsFromLocalStorage() {
+    const storedList = localStorage.getItem('todoList');
+    if (storedList) {
+        const todoList = document.getElementById("todoList");
+        todoList.innerHTML = JSON.parse(storedList).join('');
+        addEventListenersToItems();
+    }
+}
+
+function addEventListenersToItems() {
+    const items = document.querySelectorAll("#todoList li");
+    items.forEach(item => {
+        const checkbox = item.querySelector('input[type="button"]');
+        checkbox.addEventListener('click', function () {
+            checkbox.classList.toggle('clicked');
+            updateLocalStorage();
+        });
+
+        const removeBtn = item.querySelector('button');
+        removeBtn.addEventListener('click', function () {
+            item.remove();
+            updateLocalStorage();
+        });
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    loadItemsFromLocalStorage();
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    updateCurrentDateTime();
+});
+
+function addItem() {
+    const input = document.getElementById("inputi");
+    const inputValue = input.value.trim();
+
+    if (inputValue.length > 1) {
+        const li = document.createElement('li');
+        const containerDiv = document.createElement('div');
+        const textDiv = document.createElement('div'); 
+        const dateDiv = document.createElement('div'); 
+        const checkboxDiv = document.createElement('div');
+        const checkbox = document.createElement('input');
+        checkbox.type = 'button';
+        checkbox.value = ''; 
+        const removeBtn = document.createElement('button');
+        removeBtn.innerText = '';
+        removeBtn.onclick = function () {
+            li.remove();
+            updateLocalStorage();
+        };
+
+        checkboxDiv.appendChild(checkbox);
+        checkboxDiv.appendChild(removeBtn);
+
+        const currentDate = new Date();
+        const hours = currentDate.getHours();
+        const minutes = currentDate.getMinutes();
+        const amOrPm = hours >= 12 ? 'PM' : 'AM';
+        const formattedDate = `Today at ${formatTime(hours, minutes)} ${amOrPm}`;
+
+        dateDiv.textContent = formattedDate;
+        dateDiv.classList.add('item-date');
+
+        textDiv.appendChild(document.createTextNode(inputValue));
+
+        containerDiv.appendChild(textDiv);
+        containerDiv.appendChild(dateDiv);
+
+        li.appendChild(containerDiv);
+        li.appendChild(checkboxDiv);
+
+        checkbox.addEventListener('click', function () {
+            checkbox.classList.toggle('clicked');
+            updateLocalStorage();
+        });
+
+        document.getElementById("todoList").appendChild(li);
+        updateLocalStorage();
+
+        input.value = "";
+    }
+}
+
+function formatTime(hours, minutes) {
+    const formattedHours = (hours % 12 || 12).toString().padStart(2, '0');
+    const formattedMinutes = minutes.toString().padStart(2, '0');
+    return `${formattedHours}:${formattedMinutes}`;
 }
